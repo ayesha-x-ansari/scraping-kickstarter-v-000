df351306e5590ae7875dd4e501ddda8f8421b437
ayesha ansari <ayesha.x.ansari@gmail.com>
2018-04-26 22:36:11 +0000

    automatically backed up by learn

--- kick.rb
+++ kick.rb
@@ -14,7 +14,7 @@ def create_project_hash
       :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
     }
   end
-   puts projects[:image_link]
+   puts projects[image_link]
    puts projects[:percent_funded]
   # return the projects
   projects
